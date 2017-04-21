664994c0911c9302efab131ebd5b1f06e1652fff
Federico Giuggioloni <federico.giuggioloni@gmail.com>
2017-04-21 11:06:12 +0200

    cleanup

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -7,14 +7,14 @@ EVAL=$4
 FOLDER=$5
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+# 2step 2stepgivenc 3step gen_nofeats
 for METHOD in witten_bell absolute kneser_ney katz presmoothed unsmoothed
 do
-    for ORDER in 2 3
+    for ORDER in 2 3 4 5
     do
-        for CUTOFF in 0
+        for CUTOFF in 0 1
         do
-            for TAGGER in nofeats 2step 2stepgivenc 3step gen_nofeats
+            for TAGGER in nofeats
             do
                 $DIR/create_and_run.sh -i $TRAIN -f $FEATS -t $TEST -s $TAGGER -b $FOLDER/$TAGGER-$METHOD-$ORDER-cutoff_$CUTOFF -j 3 -e $EVAL -c $CUTOFF -m $METHOD -o $ORDER
             done
